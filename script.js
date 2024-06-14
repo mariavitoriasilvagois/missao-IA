e769e9795f00d54f5fc6289cfe7257605a911bd4
mariavitoriasilvagois <108538577+mariavitoriasilvagois@users.noreply.github.com>
2024-06-14 11:36:32 -0300

    jyj

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-principal");
+const caixaAlternativas = document.querySelector(".caixa-principal");
+const caixaResultados = document.querySelector(".caixa-principal");
+const caixaResultados = document.querySelector(".caixa-principal");
+
+const = perguntas [
+    {
+        enunciado: "Pergunta 1"
+        alternativas: [
+            "Alternativa 1" ,
+            "Alternativa 2"
+    ]
+    },
+    {
+        enunciado: "Pergunta 2",
+        alterntivas: [
+            "Alternativa 1" ,
+            "Alternativa 2"
+    ]
+    }
+];
